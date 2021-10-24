d1e324860b39f261200ec1f1fb67af212283e201
Terrence Meikle <terrence_meikle@yahoo.com>
2021-10-24 14:41:24 -0400

    adding express note taker, will also be updloaded to heroku

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const fs = require("fs");
+// const notes = require("./db/db.json");
+const path = require("path");
+const uuid = require("uuid");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+app.post("/api/notes", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+  const newNotes = req.body;
+  newNotes.id = uuid.v4();
+  notes.push(newNotes);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes), null, 5);
+  res.json(notes);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+  const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+  fs.writeFileSync("./db/db.json", JSON.stringify(delNote), null, 5);
+  res.json(delNote);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("Server is listening" + PORT);
+});
